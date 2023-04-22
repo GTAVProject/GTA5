849caebfb2a313a52547c818f78927e42baca3a0
haihp15 <130484610+haihp15@users.noreply.github.com>
2023-04-23 01:33:00 +0700

    add code

---
+++ Assets/Scripts/Rifle/Handgun.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Handgun : MonoBehaviour
+{
+    [Header("Rifle things")]
+    public Camera cam;
+    public float giveDamage = 10f;
+    public float shootingRange = 100f;
+
+    private void Update()
+    {
+        if (Input.GetbuttonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+  void Shoot()
+    {
+        RaycastHit hitInfo;
+
+        if(Physics.Raycast(cam.transform.position, cam.transform.forward, out hitInfo,shoottingRange))
+        {
+            Debug.Log(hitInfo.transform.name);
+        }
+    }
+}
