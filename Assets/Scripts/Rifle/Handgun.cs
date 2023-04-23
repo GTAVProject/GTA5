fbb0994301ce84e9814f69dcfc0b0bece5a71185
haihp15 <130484610+haihp15@users.noreply.github.com>
2023-04-23 11:00:45 +0700

    FIX

--- Assets/Scripts/Rifle/Handgun.cs
+++ Assets/Scripts/Rifle/Handgun.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Handgun : MonoBehaviour
 {
-    [Header("Rifle things")]
+    [Header("Rifle Things")]
     public Camera cam;
     public float giveDamage = 10f;
     public float shootingRange = 100f;
@@ -21,7 +21,7 @@ public class Handgun : MonoBehaviour
     {
         RaycastHit hitInfo;
 
-        if(Physics.Raycast(cam.transform.position, cam.transform.forward, out hitInfo,shoottingRange))
+        if(Physics.Raycast(cam.transform.position, cam.transform.forward, out hitInfo, shoottingRange))
         {
             Debug.Log(hitInfo.transform.name);
         }
